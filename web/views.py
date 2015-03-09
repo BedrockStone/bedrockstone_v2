73fd9d68a0a95724c67b39c441bfec5f0e47f4b3
Elliott O'Hara <elliott.ohara@gmail.com>
2015-03-09 09:06:31 -0500

    clean up

--- web/views.py
+++ web/views.py
@@ -29,6 +29,11 @@ class JobDetail(DetailView):
 def virtual_showcase(request):
     return render(request, 'web/virtual_showcase.html')
 
+def location(request,name):
+    l = get_object_or_404(Location, slug=name)
+    context = {'location': l}
+    return render(request, 'web/location.html', context)
+
 
 class Products(ListView):
     queryset = Category.objects.all()
