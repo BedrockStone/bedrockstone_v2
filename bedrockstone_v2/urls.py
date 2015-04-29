fbd97d0df4bf63507d5a30621b79ea18f571ef3c
Elliott O'Hara <elliott.ohara@gmail.com>
2015-04-29 08:19:53 -0500

    launch bug fixes

--- bedrockstone_v2/urls.py
+++ bedrockstone_v2/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 from bedrockstone_v2 import settings
 from web import views
@@ -13,6 +14,7 @@ urlpatterns = patterns('', url(r'^$', views.index, name='index'),
                        url(r'^admin/', include(admin_site.urls), name="bedrock"),
                        url(r'^products/$', views.Products.as_view(), name='products'),
                        url(r'^products/(.+)/$', views.CategoryDetail.as_view(), name='category'),
+                       url(r'^patio.html', RedirectView.as_view(url='products/patio',permanent=True)),
                        url(r'^about/$', views.aboutus, name='about'),
                        url(r'^contact/$', views.ContactUs.as_view(), name='contact'),
                        url(r'^jobs/$', views.Jobs.as_view(), name='jobs'),
@@ -20,7 +22,6 @@ urlpatterns = patterns('', url(r'^$', views.index, name='index'),
                        url(r'^showcase/$', views.VirtualShowcase.as_view(), name='showcase'),
                        url(r'^showcase/(.+)/$', views.Project.as_view(), name='project_type'),
                        url(r'^(.+)/', views.Content.as_view(), name='content'),
-                     
                        #NOT FOR PROD
                        url(r'^static/media/(?P<path>,*)$', 'django.views.static.serve',
                         {'document_root': settings.MEDIA_ROOT}),)
