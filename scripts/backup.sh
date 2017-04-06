e98427cac6384f822cb1a94a328b19ff31bf05a6
Elliott O'Hara <elliott.ohara@gmail.com>
2017-04-06 12:00:07 -0500

    don't delete file until done

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,7 +4,7 @@ export DATABASE_SCHEMA_NAME=bedrock
 export S3_BUNDLE=austinrockyard.com
 mysqldump -ubedrockweb -pbedrock99922  $DATABASE_SCHEMA_NAME > temp.sql
 gzip temp.sql
-rm -rf $BACKUP_FILE
 mv temp.sql.gz $BACKUP_FILE
 export S3NAME=`date +"%Y%m%d"`
 s3cmd put $BACKUP_FILE s3://$S3_BUNDLE/BACKUPS/$S3NAME.sql.gz
+rm -rf $BACKUP_FILE
