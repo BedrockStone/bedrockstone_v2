5b40ee680a3adf73744bd342c515a5e7f6b25d43
Elliott O'Hara <elliott.ohara@gmail.com>
2017-04-06 11:52:29 -0500

    added scripts for maintenance tasks

---
+++ scripts/backup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+export BACKUP_FILE=bedrock.backup.sql.gz
+export DATABASE_SCHEMA_NAME=bedrock
+export S3_BUNDLE=austinrockyard.com
+mysqldump -ubedrockweb -pbedrock99922  $DATABASE_SCHEMA_NAME > temp.sql
+gzip temp.sql
+rm -rf $BACKUP_FILE
+mv temp.sql.gz $BACKUP_FILE
+export S3NAME=`date +"%Y%m%d"`
+s3cmd put $BACKUP_FILE s3://$S3_BUNDLE/BACKUPS/$S3NAME.sql.gz
